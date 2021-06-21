d93ba3ee9aeb081e945473e3d2caff2305ae4c29
Wyatt Jacob Herkamp <wherkamp@kingtux.me>
2021-06-20 20:14:24 -0400

    Added Home Support

--- src/main/java/me/kingtux/dynmapessentials/DynmapEssentials.java
+++ src/main/java/me/kingtux/dynmapessentials/DynmapEssentials.java
@@ -1,12 +1,14 @@
 package me.kingtux.dynmapessentials;
 
 import com.earth2me.essentials.Essentials;
+import com.earth2me.essentials.User;
 import com.earth2me.essentials.Warps;
 import com.earth2me.essentials.commands.WarpNotFoundException;
 import net.ess3.api.InvalidWorldException;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -15,8 +17,11 @@ import org.dynmap.markers.Marker;
 import org.dynmap.markers.MarkerAPI;
 import org.dynmap.markers.MarkerSet;
 
+import java.util.List;
+
 public final class DynmapEssentials extends JavaPlugin implements Runnable, Listener {
     private Warps essentialsWarps;
+    private Essentials essentials;
     private DynmapAPI dynmapAPI;
     private MarkerAPI markerAPI;
 
@@ -24,7 +29,8 @@ public final class DynmapEssentials extends JavaPlugin implements Runnable, List
     public void onEnable() {
         PluginManager pluginManager = Bukkit.getPluginManager();
         if (pluginManager.isPluginEnabled("Essentials")) {
-            essentialsWarps = ((Essentials) pluginManager.getPlugin("Essentials")).getWarps();
+            essentials = ((Essentials) pluginManager.getPlugin("Essentials"));
+            essentialsWarps = essentials.getWarps();
         } else {
             getLogger().severe("Plugin Can not load without Essentials");
             pluginManager.disablePlugin(this);
@@ -35,6 +41,7 @@ public final class DynmapEssentials extends JavaPlugin implements Runnable, List
             getLogger().severe("Plugin Can not load without Essentials");
             pluginManager.disablePlugin(this);
         }
+        saveDefaultConfig();
         markerAPI = dynmapAPI.getMarkerAPI();
         Metrics metrics = new Metrics(this, 9786);
         //Rerun this every minute
@@ -48,6 +55,15 @@ public final class DynmapEssentials extends JavaPlugin implements Runnable, List
 
     @Override
     public void run() {
+        if (getConfig().getBoolean("show-warps")) {
+            runWarps();
+        }
+        if (getConfig().getBoolean("show-homes")) {
+            runHomes();
+        }
+    }
+
+    private void runWarps() {
         MarkerSet markerSet = markerAPI.getMarkerSet("Essentials_Warps");
         if (markerSet == null) {
             markerSet = markerAPI.createMarkerSet("Essentials_Warps", "Warps", null, false);
@@ -68,6 +84,35 @@ public final class DynmapEssentials extends JavaPlugin implements Runnable, List
                 marker.setLabel(s);
                 marker.setMarkerIcon(markerAPI.getMarkerIcon("pin"));
                 marker.setLocation(location.getWorld().getName(), location.getX(), location.getY(), location.getZ());
+            }
+        }
+    }
+
+    private void runHomes() {
+        MarkerSet markerSet = markerAPI.getMarkerSet("Essentials_Homes");
+        if (markerSet == null) {
+            markerSet = markerAPI.createMarkerSet("Essentials_Homes", "Homes", null, false);
+        }
+        for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+            User user = essentials.getUser(onlinePlayer);
+            List<String> homes = user.getHomes();
+            for (String home : homes) {
+                String homeName = new StringBuilder(onlinePlayer.getName()).append(": ").append(home).toString();
+                try {
+                    Location location = user.getHome(home);
+
+                    if (markerSet.findMarkerByLabel(homeName) != null) {
+                        //Update the Location
+                        markerSet.findMarkerByLabel(homeName).setLocation(location.getWorld().getName(), location.getX(), location.getY(), location.getZ());
+                    } else {
+                        Marker marker = markerSet.createMarker(null, homeName, location.getWorld().getName(), location.getX(), location.getY(), location.getZ(), markerAPI.getMarkerIcon("pin"), false);
+                        marker.setLabel(homeName);
+                        marker.setMarkerIcon(markerAPI.getMarkerIcon("bed"));
+                        marker.setLocation(location.getWorld().getName(), location.getX(), location.getY(), location.getZ());
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
