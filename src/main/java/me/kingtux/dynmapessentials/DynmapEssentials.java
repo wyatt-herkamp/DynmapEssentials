e0bb2233807ed5152f7274ced62589873e66f70f
Wyatt J Herkamp <wherkamp@kingtux.me>
2021-12-01 00:24:24 -0500

    Added support for custom markers

--- src/main/java/me/kingtux/dynmapessentials/DynmapEssentials.java
+++ src/main/java/me/kingtux/dynmapessentials/DynmapEssentials.java
@@ -87,7 +87,7 @@ public final class DynmapEssentials extends JavaPlugin implements Runnable, List
                 //Update the Location
                 markerSet.findMarkerByLabel(s).setLocation(location.getWorld().getName(), location.getX(), location.getY(), location.getZ());
             } else {
-                Marker marker = markerSet.createMarker(null, s, location.getWorld().getName(), location.getX(), location.getY(), location.getZ(), markerAPI.getMarkerIcon(getConfig().getString("home.marker","pin")), false);
+                Marker marker = markerSet.createMarker(null, s, location.getWorld().getName(), location.getX(), location.getY(), location.getZ(), markerAPI.getMarkerIcon(getConfig().getString("warp.marker","pin")), false);
                 marker.setLabel(s);
                 marker.setMarkerIcon(markerAPI.getMarkerIcon("pin"));
                 marker.setLocation(location.getWorld().getName(), location.getX(), location.getY(), location.getZ());
