4a00442b29010cf5ff49440d3f589082a5d109f5
Wyatt Herkamp <wherkamp@gmail.com>
2023-12-21 14:39:35 -0500

    Cleanup the code

--- src/main/kotlin/dev/kingtux/dynmapessentials/DynmapEssentials.kt
+++ src/main/kotlin/dev/kingtux/dynmapessentials/DynmapEssentials.kt
@@ -12,13 +12,12 @@ import org.bukkit.plugin.java.JavaPlugin
 import org.dynmap.DynmapAPI
 import org.dynmap.markers.MarkerAPI
 
-class DynmapEssentials : JavaPlugin(), Runnable, Listener {
-    private lateinit var essentialsWarps: Warps;
-    private lateinit var  essentials: Essentials
-    private lateinit var  dynmapAPI: DynmapAPI
-    private lateinit var  markerAPI: MarkerAPI
+class DynmapEssentials : JavaPlugin() {
+
 
     override fun onEnable() {
+        val essentials: Essentials
+        val dynmapAPI: DynmapAPI
         val pluginManager = Bukkit.getPluginManager()
         if (pluginManager.isPluginEnabled("Essentials")) {
             val essentialsPlugin = pluginManager.getPlugin("Essentials");
@@ -27,10 +26,10 @@ class DynmapEssentials : JavaPlugin(), Runnable, Listener {
                 return;
             }
             essentials = essentialsPlugin as Essentials
-            essentialsWarps = essentials.warps;
         } else {
             logger.severe("Plugin Can not load without Essentials")
             pluginManager.disablePlugin(this)
+            return;
         }
         if (pluginManager.isPluginEnabled("dynmap")) {
             val dynmapPlugin = pluginManager.getPlugin("dynmap");
@@ -42,105 +41,32 @@ class DynmapEssentials : JavaPlugin(), Runnable, Listener {
         } else {
             logger.severe("Plugin Can not load without Essentials")
             pluginManager.disablePlugin(this)
+            return;
         }
+
         saveDefaultConfig()
         config.addDefault("show-homes", false)
         config.addDefault("show-warps", true)
         config.addDefault("home.show-offline", false)
         config.addDefault("home.format", "{name}: {home}")
         saveConfig()
-        markerAPI = dynmapAPI.markerAPI
+        // Update From 1.2 to 1.3
+        if (dynmapAPI.markerAPI.getMarkerSet("Essentials_Homes") != null) {
+            logger.info("Old Marker Set Found. Deleting. New V1.3 uses has Ids")
+            dynmapAPI.markerAPI.getMarkerSet("Essentials_Homes").deleteMarkerSet()
+        }
+        if (dynmapAPI.markerAPI.getMarkerSet("Essentials_Warps") != null) {
+            logger.info("Old Marker Set Found. Deleting. New V1.3 uses has Ids")
+            dynmapAPI.markerAPI.getMarkerSet("Essentials_Warps").deleteMarkerSet()
+        }
         val metrics = Metrics(this, 9786)
         //Rerun this every minute
-        server.scheduler.runTaskTimer(this, this, 0, config.getInt("refresh-rate", 1200).toLong())
+        server.scheduler.runTaskTimer(this, MarkerUpdater(this, essentials, dynmapAPI), 0, config.getInt("refresh-rate", 1200).toLong())
     }
 
     override fun onDisable() {
     }
 
-    override fun run() {
-        if (config.getBoolean("show-warps")) {
-            runWarps()
-        }
-        if (config.getBoolean("show-homes")) {
-            runHomes()
-        }
-    }
 
-    private fun runWarps() {
-        var markerSet = markerAPI.getMarkerSet("Essentials_Warps")
-        if (markerSet == null) {
-            markerSet = markerAPI.createMarkerSet("Essentials_Warps", "Warps", null, false)
-        }
-        for (s in essentialsWarps.list) {
-            var location: Location
-            try {
-                location = essentialsWarps.getWarp(s)
-            } catch (e: WarpNotFoundException) {
-                logger.warning("WarpNotFound but is in list. ${e.message}")
-                continue
-            } catch (e: InvalidWorldException) {
-                logger.warning("Invalid World: ${e.message}")
-                continue
-            }
-            if (markerSet.findMarkerByLabel(s) != null) {
-                //Update the Location
-                markerSet.findMarkerByLabel(s).setLocation(location.world!!.name, location.x, location.y, location.z)
-            } else {
-                val icon = markerAPI.getMarkerIcon(config.getString("warp.marker", "pin"))
-                val marker = markerSet.createMarker(
-                    null,
-                    s,
-                    location.world!!.name,
-                    location.x,
-                    location.y,
-                    location.z,
-                    icon,
-                    false
-                )
-                marker.label = s
-                marker.setMarkerIcon(icon)
-                marker.setLocation(location.world!!.name, location.x, location.y, location.z)
-            }
-        }
-    }
 
-    private fun runHomes() {
-        var markerSet = markerAPI.getMarkerSet("Essentials_Homes")
-        if (markerSet == null) {
-            markerSet = markerAPI.createMarkerSet("Essentials_Homes", "Homes", null, false)
-        }
-
-        for (uuid in essentials.userMap.allUniqueUsers) {
-            val user = essentials.getUser(uuid)
-            val offlinePlayer = Bukkit.getOfflinePlayer(uuid)
-            if (!offlinePlayer.isOnline && !config.getBoolean("home.show-offline")) continue
-            val homes = user.homes
-            for (home in homes) {
-                val homeName = config.getString("home.format")!!
-                    .replace("{name}", offlinePlayer.name!!).replace("{home}", home)
-                try {
-                    val location = user.getHome(home)
-
-                    if (markerSet.findMarkerByLabel(homeName) != null) {
-                        //Update the Location
-                        markerSet.findMarkerByLabel(homeName)
-                            .setLocation(location.world!!.name, location.x, location.y, location.z)
-                    } else {
-                        val icon = markerAPI.getMarkerIcon(config.getString("home.marker", "pin"))
-                        val marker = markerSet.createMarker(
-                            null, homeName, location.world!!
-                                .name, location.x, location.y, location.z, icon, false
-                        )
-                        marker.label = homeName
-                        marker.setMarkerIcon(icon)
-                        marker.setLocation(location.world!!.name, location.x, location.y, location.z)
-                    }
-                } catch (e: Exception) {
-                    logger.warning("Unknown Error with $homeName")
-                    logger.warning(e.message)
-                }
-            }
-        }
-    }
 }
---
+++ src/main/kotlin/dev/kingtux/dynmapessentials/MarkerUpdater.kt
@@ -0,0 +1,131 @@
+package dev.kingtux.dynmapessentials
+
+import com.earth2me.essentials.Essentials
+import com.earth2me.essentials.Warps
+import com.earth2me.essentials.commands.WarpNotFoundException
+import net.ess3.api.InvalidWorldException
+import org.bukkit.Bukkit
+import org.bukkit.Location
+import org.bukkit.configuration.file.FileConfiguration
+import org.dynmap.DynmapAPI
+import org.dynmap.markers.MarkerAPI
+data class MarkerConfig(
+    val showHomes: Boolean,
+    val showWarps: Boolean,
+    val showOffline: Boolean,
+    val homeFormat: String,
+    val homeMarker: String,
+    val warpMarker: String
+){
+    constructor(config: FileConfiguration) : this(
+        config.getBoolean("show-homes"),
+        config.getBoolean("show-warps"),
+        config.getBoolean("home.show-offline"),
+        config.getString("home.format", "{name}: {home}")!!,
+        config.getString("home.marker", "pin")!!,
+        config.getString("warp.marker", "pin")!!,
+    )
+}
+class MarkerUpdater(
+    private var essentials: Essentials,
+    private var dynmapAPI: DynmapAPI,
+    private var markerAPI: MarkerAPI,
+    private var essentialsWarps: Warps,
+    private var plugin: DynmapEssentials,
+    private var config: MarkerConfig
+) : Runnable {
+    constructor(plugin: DynmapEssentials, essentials: Essentials, dynmapAPI: DynmapAPI) : this(
+        essentials,
+        dynmapAPI,
+        dynmapAPI.markerAPI,
+        essentials.warps,
+        plugin,
+        MarkerConfig(plugin.config)
+    )
+
+    private fun updateWarps() {
+        val icon = markerAPI.getMarkerIcon(config.warpMarker)
+
+        var markerSet = markerAPI.getMarkerSet("Essentials_Warps_V1_3")
+        if (markerSet == null) {
+            markerSet = markerAPI.createMarkerSet("Essentials_Warps_V1_3", "Warps", null, false)
+        }
+        for (s in essentialsWarps.list) {
+            var location: Location
+            try {
+                location = essentialsWarps.getWarp(s)
+            } catch (e: WarpNotFoundException) {
+                plugin.logger.warning("WarpNotFound but is in list. ${e.message}")
+                continue
+            } catch (e: InvalidWorldException) {
+                plugin.logger.warning("Invalid World: ${e.message}")
+                continue
+            }
+            val markerId = "${s}_warp"
+            if (markerSet.findMarker(markerId) != null) {
+                //Update the Location
+                markerSet.findMarker(markerId).let {
+                    it.setLocation(location.world!!.name, location.x, location.y, location.z)
+                    it.markerIcon = icon
+                }
+            } else {
+                 markerSet.createMarker(
+                    markerId,
+                    s,
+                    location.world!!.name,
+                    location.x,
+                    location.y,
+                    location.z,
+                    icon,
+                    false
+                )
+            }
+        }
+    }
+
+    private fun updateHomes() {
+        val icon = markerAPI.getMarkerIcon(config.homeMarker)
+        var markerSet = markerAPI.getMarkerSet("Essentials_Homes_V1_3")
+        if (markerSet == null) {
+            markerSet = markerAPI.createMarkerSet("Essentials_Homes_V1_3", "Homes", null, false)
+        }
+        for (uuid in essentials.userMap.allUniqueUsers) {
+            val user = essentials.getUser(uuid)
+            val offlinePlayer = Bukkit.getOfflinePlayer(uuid)
+            if (!offlinePlayer.isOnline && !config.showOffline) continue
+            val homes = user.homes
+            for (home in homes) {
+                val homeId = "${offlinePlayer.uniqueId}_$home"
+                val homeName = config.homeFormat
+                    .replace("{name}", offlinePlayer.name!!).replace("{home}", home)
+                val location = runCatching { user.getHome(home) }.getOrNull();
+                if (location == null) {
+                    plugin.logger.warning("Home not found but is in list. ${user.name} $home")
+                    continue
+                }
+                if (markerSet.findMarker(homeId) != null) {
+                    //Update the Location
+                    markerSet.findMarker(homeName).let {
+                        it.setLocation(location.world!!.name, location.x, location.y, location.z)
+                        it.markerIcon = markerAPI.getMarkerIcon(config.homeMarker)
+                    }
+                } else {
+                    markerSet.createMarker(
+                        homeId, homeName, location.world!!
+                            .name, location.x, location.y, location.z, icon, false
+                    )
+                }
+
+            }
+        }
+    }
+
+    override fun run() {
+        if (config.showWarps) {
+            updateWarps()
+        }
+        if (config.showHomes) {
+            updateHomes()
+        }
+    }
+}
